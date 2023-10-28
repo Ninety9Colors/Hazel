4623d8f35b40c90df5f865d162c2aea8efc35b53
Ninety9Colors <darekzhang0629@gmail.com>
2023-10-27 23:14:23 -0500

    Added spdlog and basic logging functionality

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,11 +1,14 @@
 #ifdef HZ_PLATFORM_WINDOWS
-
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, int** argv) {
-	auto app = Hazel::CreateApplication();
+	Hazel::Log::init();
+	HZ_CORE_WARN("Initialized Logger!");
+
+	Hazel::Application* app = Hazel::CreateApplication();
 	app->run();
 	delete app;
+
 }
 
 #endif // HZ_PLATFORM_WINDOWS
